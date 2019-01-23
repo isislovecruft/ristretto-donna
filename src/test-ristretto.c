0a1eadcf992bda4d4588709a76a364523baa42e2
Isis Lovecruft <isis@patternsinthevoid.net>
2019-01-23 21:39:31 +0000

    Add printed output for tests.

--- src/test-ristretto.c
+++ src/test-ristretto.c
@@ -6,6 +6,7 @@
 // - isis agora lovecruft <isis@patternsinthevoid.net>
 
 #include <stdint.h>
+#include <stdio.h>
 
 #include "ristretto-donna.h"
 
@@ -15,6 +16,13 @@ int test_ristretto_decode_basepoint()
   uint8_t result;
 
   result = ristretto_decode(&point, RISTRETTO_BASEPOINT_COMPRESSED);
+
+  if (result != 1) {
+    printf("could not decode basepoint\n");
+  }
+
+  return (int)result;
+}
 
   return (int)result;
 }
